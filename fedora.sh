25aaa452f0bcdb2a4d263b91cd5710503fa6d222
Giuseppe Tinti Tomio <67562667+GiuseppeTT@users.noreply.github.com>
2021-05-22 16:17:55 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -27,6 +27,7 @@ flatpak install flathub \
 
 # Install VSCode
 # https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
+# Remember to change title bar settings: https://github.com/microsoft/vscode/issues/43154
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
